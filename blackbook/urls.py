29ba24fd24ee774208a6af8b4ff4a94c43b31b4b
Lukas Sobik <hukash@gmail.com>
2011-02-14 17:01:36 +0100

    add navigation in videos, add default intro for photos

--- blackbook/urls.py
+++ blackbook/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('blackbook.views',
 	(r'^$', 'index'),
 	(r'^videos/$', 'show_video'),
 	(r'^video/(?P<slug>[-\w]+)/$', 'show_video'),
+    (r'^category/$', 'show_category'),
     (r'^category/(?P<slug>[-\w]+)/$', 'show_category'),
 )
--- blackbook/views.py
+++ blackbook/views.py
@@ -9,15 +9,18 @@ def index(request):
     context = Context({'object_list': categories})
     return HttpResponse(template.render(context))
 
-def show_category(request, slug):
-    photos = Photo.objects.filter(
-                category__slug__exact=slug
-             ).filter(
-                active=True
-             )
+def show_category(request, slug=None):
     try:
-        category = Category.objects.get(slug=slug)
         categories = Category.objects.all()
+        if slug:
+            category = Category.objects.get(slug=slug)
+        else:
+            category = Category.objects.all()[0]
+        photos = Photo.objects.filter(
+                    category__slug__exact=category.slug
+                 ).filter(
+                    active=True
+                 )
     except Category.DoesNotExist:
         raise Http404
     template = loader.get_template('blackbook/show-category.html')
@@ -25,8 +28,8 @@ def show_category(request, slug):
     return HttpResponse(template.render(context))
 
 def show_video(request, slug=None):
-    videos = Video.objects.all()
     try:
+        videos = Video.objects.all()
         if slug: 
             video = Video.objects.get(slug=slug)
         else:
